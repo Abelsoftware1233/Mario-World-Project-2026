fc8d3e1e6d5e336b7176f9e283233fc322de75a2
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-21 19:11:39 +0100

    Update Bowser.js

--- js/mainGame/Bowser.js
+++ js/mainGame/Bowser.js
@@ -1,8 +1,8 @@
-function Bowser() {
+Function Bowser() {
   var gameUI = GameUI.getInstance();
 
-  var tickCounter = 0; //for animating enemy
-  var maxTick = 10; //max number for ticks to show enemy sprite
+  var tickCounter = 0; // for animating enemy
+  var maxTick = 10; // max number for ticks to show enemy sprite
 
   var element = new Image();
   element.src = 'images/bowser.png';
@@ -17,20 +17,25 @@ function Bowser() {
 
   this.sX;
   this.sY = 0;
-  this.width = 32;
-  this.height = 32;
+  
+  // 💥 CORRECTIE: De afmetingen zijn aangepast naar de waarden van de CSS (40x34)
+  this.width = 40; 
+  this.height = 34;
 
   this.frame = 0;
 
   var that = this;
 
-  this.goomba = function() {
-    this.type = 20;
+  // 💥 CORRECTIE: De 'goomba'-functie is hernoemd naar 'init' en krijgt 
+  // een uniek 'type' (21) toegewezen voor Bowser.
+  this.init = function() { 
+    this.type = 21; // Uniek type voor Bowser
     that.sX = 0;
   };
 
   this.draw = function() {
     that.sX = that.width * that.frame;
+    // Gebruikt de gecorrigeerde afmetingen van 40x34 voor tekenen
     gameUI.draw(element, that.sX, that.sY, that.width, that.height, that.x, that.y, that.width, that.height);
   };
 
@@ -42,24 +47,27 @@ function Bowser() {
     }
 
     if (that.state == 'dead') {
-      that.frame = 2; //squashed goomba
+      // ⚠️ LET OP: Dit is nog steeds Goomba's doodslogica.
+      // Als Bowser een specifieke doodsanimatie heeft (bijv. vlammen of exploderen),
+      // moet de logica hier worden aangepast (bijv. andere frame-indices).
+      that.frame = 2; // (Dit kan een 'squashed' frame zijn als je het zo wilt)
 
       tickCounter++;
       if (tickCounter >= 60) {
         that.frame = 4;
       }
     } else if (that.state == 'deadFromBullet') {
-      //falling goomba
-      that.frame = 3;
+      // ⚠️ LET OP: Dit is nog steeds Goomba's vallende logica.
+      that.frame = 3; // (Dit kan een 'vallend' frame zijn)
       that.velY += gravity;
       that.y += that.velY;
     } else {
-      //only animate when not dead
+      // Normale loop- en zwaartekrachtlogica
       that.velY += gravity;
       that.x += that.velX;
       that.y += that.velY;
 
-      //for animating
+      // for animating (tussen frame 0 en 1)
       tickCounter += 1;
 
       if (tickCounter > maxTick) {
